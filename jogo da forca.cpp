a6fb291d52ca6d496281a0d768460d108feb1794
DellyPlays <132455461+Guilherme-silva-teixeira@users.noreply.github.com>
2024-05-09 12:50:12 -0300

    Update jogo da forca.cpp

--- jogo da forca.cpp
+++ jogo da forca.cpp
@@ -12,6 +12,7 @@ int main()
 	bool acerto;
 	chances = 6;
 	tam = 0;
+	i = 0;
 	acerto = false;
 	acertos = 0;
 	std::cout << "Fale para o seu amigo fechar os olhos e digite a palavra secreta: " << std::endl;
@@ -26,10 +27,41 @@ int main()
 	{
 		secreta[i] = '-';
 	}
-	while ()
+	while ((chances > 0)&&(acertos < tam))
 	{
-
+		std::cout << "Chances restantes: " << chances << "\n\n" << std::endl;
+		std::cout << "Palavra secreta: " << std::endl;
+		for (i = 0; i < tam; i++)
+		{
+			std::cout << secreta[i] << std::endl;
+		}
+		std::cout << "\n\nDigite uma letra: " << std::endl;
+		std::cin >> letra[0];
+		for (i = 0; i < tam; i++)
+		{
+			if (palavra[i] == letra[0])
+			{
+				acerto = true;
+				secreta[i] = palavra[i];
+				acertos++;
+			}
+		}
+		if (!acerto)
+		{
+			chances--;
+		}
+		acerto = false;
+		system("cls");
 	}
-	
+	if (acertos == tam)
+	{
+		std::cout << "Parabéns, você venceu" << std::endl;
+	}
+	else
+	{
+		std::cout << "Que pena, você perdeu" << std::endl;
+	}
+	system("pause");
+
 	return 0;
 }
