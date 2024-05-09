ff5052ad7fe561d6fab49fe78b158cd29d3efcdd
DellyPlays <132455461+Guilherme-silva-teixeira@users.noreply.github.com>
2024-05-08 21:05:55 -0300

    Update test2.cpp

--- test2.cpp
+++ test2.cpp
@@ -12,20 +12,21 @@ int main()
 	int c = 0;
 	int l = 0;
 	int n = 1;
+	int ln, cn;
 	int matrix[3][3];
 	for (c; c < 3; c++)
 	{
 		for (l; l < 3; l++)
 		{
-			matrix[c][l] += n;
+			matrix[c][l] = n;
 			n++;
 		}
 	}
 	std::cout << "Valores de array aramzenados com sucesso\n" << std::endl;
 	std::cout << "Digite a linha de um array (máximo 3)" << std::endl;
-	std::cin >> l;
+	std::cin >> ln;
 	std::cout << "Agora digite o número da coluna do array" << std::endl;
-	std::cin >> c;
-	std::cout << "O valor do array pesquisado é de: " << matrix[c][l] << "." << std::endl;
+	std::cin >> cn;
+	std::cout << "O valor do array pesquisado é de: " << matrix[cn][ln] << "." << std::endl;
 	return 0;
 }
