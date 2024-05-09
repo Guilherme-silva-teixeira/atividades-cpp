237a58d8fd072f3556ac529f4f2797b007fc3445
DellyPlays <132455461+Guilherme-silva-teixeira@users.noreply.github.com>
2024-05-08 21:03:57 -0300

    Create test2.cpp

---
+++ test2.cpp
@@ -0,0 +1,31 @@
+// esse arquivo trabalha com o teste de array e algumas funções
+
+#include <iostream>
+#include <cstdlib>
+#include <locale>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "portuguese");
+	int c = 0;
+	int l = 0;
+	int n = 1;
+	int matrix[3][3];
+	for (c; c < 3; c++)
+	{
+		for (l; l < 3; l++)
+		{
+			matrix[c][l] += n;
+			n++;
+		}
+	}
+	std::cout << "Valores de array aramzenados com sucesso\n" << std::endl;
+	std::cout << "Digite a linha de um array (máximo 3)" << std::endl;
+	std::cin >> l;
+	std::cout << "Agora digite o número da coluna do array" << std::endl;
+	std::cin >> c;
+	std::cout << "O valor do array pesquisado é de: " << matrix[c][l] << "." << std::endl;
+	return 0;
+}
