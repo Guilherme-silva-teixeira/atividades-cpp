a41f487ae78c7eae68e72adb781b2c6033c298b6
DellyPlays <132455461+Guilherme-silva-teixeira@users.noreply.github.com>
2024-05-13 20:56:53 -0300

    Update tic-tac-toe.cpp

--- tic-tac-toe.cpp
+++ tic-tac-toe.cpp
@@ -7,20 +7,39 @@
 
 using namespace std;
 
-void PRINTBOARD(char arr[2][2])
+void printBoard(char arr[2][2])
 {
-	std::cout << '|' << ' ' << arr[0][0] << '|' << ' ' << arr[0][1] << '|' << ' ' << arr[0][2] << '|' << std::endl;
+	std::cout << '|' << ' ' << arr[0][0] << '|' << arr[0][1] << '|' << ' ' << arr[0][2] << '|' << std::endl;
 	std::cout << '|' << ' ' << arr[1][0] << '|' << ' ' << arr[1][1] << '|' << ' ' << arr[1][2] << '|' << std::endl;
 	std::cout << '|' << ' ' << arr[2][0] << '|' << ' ' << arr[2][1] << '|' << ' ' << arr[2][2] << '|' << std::endl;
 }
 
 string verifyWinner(char arr[2][2])
 {
+	string texto;
 	if ((arr[0][0] == 'X' && arr[0][1] == 'X' && arr[0][2] == 'X') ||
 		(arr[1][0] == 'X' && arr[1][1] == 'X' && arr[1][2] == 'X') ||
-		(arr[2][0] == 'X' && arr[2][1] == 'X' && arr[2][2] == 'X'))
+		(arr[2][0] == 'X' && arr[2][1] == 'X' && arr[2][2] == 'X') ||
+		(arr[0][0] == 'X' && arr[1][1] == 'X' && arr[2][2] == 'X') ||
+		(arr[0][0] == 'X' && arr[1][0] == 'X' && arr[2][0] == 'X') ||
+		(arr[0][1] == 'X' && arr[1][1] == 'X' && arr[2][1] == 'X') ||
+		(arr[0][2] == 'X' && arr[1][2] == 'X' && arr[2][2] == 'X') ||
+		(arr[0][2] == 'X' && arr[1][1] == 'X' && arr[2][0] == 'X'))
 	{
-
+		texto = "playerX";
+		return texto;
+	}
+	else if ((arr[0][0] == 'X' && arr[0][1] == 'X' && arr[0][2] == 'X') ||
+			(arr[1][0] == 'X' && arr[1][1] == 'X' && arr[1][2] == 'X') ||
+			(arr[2][0] == 'X' && arr[2][1] == 'X' && arr[2][2] == 'X') ||
+			(arr[0][0] == 'X' && arr[1][1] == 'X' && arr[2][2] == 'X') ||
+			(arr[0][0] == 'X' && arr[1][0] == 'X' && arr[2][0] == 'X') ||
+			(arr[0][1] == 'X' && arr[1][1] == 'X' && arr[2][1] == 'X') ||
+			(arr[0][2] == 'X' && arr[1][2] == 'X' && arr[2][2] == 'X') ||
+			(arr[0][2] == 'X' && arr[1][1] == 'X' && arr[2][0] == 'X'))
+	{
+		texto = "playerY";
+		return texto;
 	}
 }
 
@@ -33,14 +52,14 @@ int main(int null)
 	int x = 0, y = 0;
 	char atualplayer = playerX;
 	bool isGameWin = false;
-	while (!isGameWin)
+	while (isGameWin != true)
 	{
 		std::cout << "Digite a coordenada x:" << std::endl;
 		std::cin >> x;
 		std::cout << "Digite a coordenada y:" << std::endl;
 		std::cin >> y;
 		arr[x][y] = atualplayer;
-		PRINTBOARD(arr);
+		printBoard(arr);
 		system("pause");
 		system("cls");
 		if (atualplayer == playerX)
@@ -49,13 +68,20 @@ int main(int null)
 		}
 		else
 		{
-
 			atualplayer = playerX;
 		}
-		if (verifyWinner(arr))
+		if (verifyWinner(arr) == "playerX")
 		{
-
+			isGameWin = true;
+			std::cout << "player X venceu!" << std::endl;
+		}
+		else if (verifyWinner(arr) == "playerY")
+		{
+			isGameWin = true;
+			std::cout << "player O venceu!" << std::endl;
 		}
 	}
+	std::cout << "Fim do jogo!" << std::endl;
+	
 	return null;
 }
