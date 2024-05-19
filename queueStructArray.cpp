31bb2692076e7051a6a6fc1b0bea5e89f3b576e5
DellyPlays <132455461+Guilherme-silva-teixeira@users.noreply.github.com>
2024-05-19 16:28:55 -0300

    Update queueStructArray.cpp

--- queueStructArray.cpp
+++ queueStructArray.cpp
@@ -14,9 +14,12 @@ int main(int null)
 	queue <string> produtos;
 	int counter = 0, stop = 0;
 	string blocker = "null", sair = "sair";
-	int op;
+	int op, one = 1;
 	string nome, id, preco, tipo;
-	int stq;
+	string  arrNome[17], arrID[17], arrPreco[17], arrTipo[17];
+	int stq, arrSTQ[17];
+	int qtd = 1;
+	string nArrNome, nArrID, nArrPreco, nArrTipo;
 	do
 	{
 		std::cout << "Digite a opção: " << std::endl;
@@ -27,6 +30,8 @@ int main(int null)
 		switch (op)
 		{
 			case 1:
+				if (true)
+				{
 				system("cls");
 				std::cout << "Digite o nome do produto: " << std::endl;
 				std::cin >> nome;
@@ -53,6 +58,55 @@ int main(int null)
 				std::cout << "\n\n\n\n\n\n\n###############          100% Concluido;" << std::endl;
 				system("pause");
 				system("cls");
+				qtd++;
+				nome = arrNome[qtd];
+				preco = arrPreco[qtd];
+				tipo = arrTipo[qtd];
+				id = arrID[qtd];
+				stq = arrSTQ[qtd];
+			}
+			break;
+			case 2:
+					{
+						if (true)
+						{
+							std::cout << "Pesquisa: " << std::endl;
+							system("cls");
+							std::cout << "Digite o nome do produto: " << std::endl;
+							std::cin >> nArrNome;
+							std::cout << "\n\n\n\n\n\n\n###                        5% Concluido;" << std::endl;
+							system("pause");
+							system("cls");
+							std::cout << "Digite o id do produto:" << std::endl;
+							std::cin >> nArrID;
+							std::cout << "\n\n\n\n\n\n\n######                    25% Concluido;" << std::endl;
+							system("pause");
+							system("cls");
+							std::cout << "Digite o preço do produto: " << std::endl;
+							std::cin >> nArrPreco;
+							std::cout << "\n\n\n\n\n\n\n#########                 50% Concluido;" << std::endl;
+							system("pause");
+							system("cls");
+							std::cout << "Digite o tipo do produto: " << std::endl;
+							std::cin >> nArrTipo;
+							std::cout << "\n\n\n\n\n\n\n############              75% Concluido;" << std::endl;
+							system("pause");
+							system("cls");
+							std::cout << "Digite o estoque do produto: " << std::endl;
+							std::cin >> stq;
+							std::cout << "\n\n\n\n\n\n\n###############          100% Concluido;" << std::endl;
+							system("pause");
+							system("cls");
+							for (int i = 0; i < qtd; i++)
+							{
+								if (strcmpq(nome, nArrNome) == "true")
+								{
+									std::cout << "Estoque do produto: " << arrSTQ[qtd] << ".\n" << std::endl;
+									std::cout << "=============================================" << std::endl;
+								}
+							}
+						}
+					}
 			break;
 		}
 	} while (!(strcmpq(blocker, sair) == "true" || blocker == "SAIR"));
