7c07409d6d2082db11bf8f2a73c9b9a586751f93
DellyPlays <132455461+Guilherme-silva-teixeira@users.noreply.github.com>
2024-05-29 15:19:22 -0300

    Create struct03.cpp

---
+++ struct03.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <locale>
+
+using namespace std;
+
+struct Produtos
+{
+	int changeArray(int tot)
+	{
+		if (tot == 0)
+		{
+			tot == 1;
+		}
+		return tot;
+	}
+
+	int tot = changeArray(1);
+
+	string nome[100];
+	string tipo[100];
+	int id[100];
+	int estq[100];
+};
+
+int main(int null)
+{
+
+	return null;
+}
