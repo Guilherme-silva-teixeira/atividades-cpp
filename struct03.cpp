957c54e8f7f8c81f8fd68007f64fb8be33194ee7
DellyPlays <132455461+Guilherme-silva-teixeira@users.noreply.github.com>
2024-05-29 15:57:16 -0300

    Update struct03.cpp

--- struct03.cpp
+++ struct03.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 #include <locale>
-#include <string>
-#include <list>
 
 using namespace std;
 
@@ -18,10 +16,10 @@ struct Produtos
 
 	int tot = changeArray(1);
 
-	string nome[100];
-	string tipo[100];
-	int id[100];
-	int estq[100];
+	string nome;
+	string tipo;
+	int id;
+	int estq;
 };
 
 void op()
@@ -29,6 +27,20 @@ void op()
 	setlocale(LC_ALL, "portuguese");
 	std::cout << "Digite a opção necessária: \n\n 1 - Cadastrar produto\n\n 2 - mostrar produtos\n\n 3 - Pesquisar e editar o produto" << std::endl;
 	std::cout << "opção: " << std::endl;
+}
+
+void CadastrarProduto()
+{
+	struct Produtos lista;
+	string nome1 = lista.nome;
+	string tip1 = lista.tipo;
+	int id1 = lista.id;
+	int estq1 = lista.estq;
+	setlocale(LC_ALL, "portuguese");
+	std::cout << "Digite o nome do produto: " << std::endl;
+	std::cin >> nome1;
+	std::cout << "Digite o tipo do produto:" << std::endl;
+	std::cin >> tip1;
 }
 
 int main(int null)
