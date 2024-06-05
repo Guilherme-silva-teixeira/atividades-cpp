a28b54dd2a49881f785416b6c012b0c92da6d37e
DellyPlays <132455461+Guilherme-silva-teixeira@users.noreply.github.com>
2024-06-05 18:51:25 -0300

    Update pointer01.cpp

--- pointer01.cpp
+++ pointer01.cpp
@@ -4,6 +4,29 @@
 
 using namespace std;
 
+void aula37()
+{
+	int* p;
+	int vetor[10];
+	p = &vetor[0];
+	//dar elementos a variável
+	for (int i = 0; i < 10; i++)
+	{
+		vetor[i] = i;
+	}
+	//mostrar o valor dos vetores
+	for (int i = 0; i < 10; i++)
+	{
+		std::cout << vetor[i] << std::endl;
+	}
+	//mostrar a posição do vetor
+	for (int i = 0; i < 10; i++)
+	{
+		std::cout << *p;
+	}
+	std::cout << "Endereço de memória: " << p << std::endl;
+}
+
 void mostrar()
 {
 	int* pn;//sem endereço
@@ -21,7 +44,8 @@ int main(int null)
 	std::cout << variavel << std::endl;
 	std::cout << "Sua alocação de memória é de: " << pv << std::endl;
 	*pv = "String trocada";
-	std::cout << "\n\n" << variavel << " \n" << *pv;
+	std::cout << "\n\n" << variavel << " \n" << *pv<< std::endl;
+	aula37();
 	mostrar();
 	return null;
 }
